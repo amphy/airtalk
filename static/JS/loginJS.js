749b7ff979f4e761b7c4affbe203391916d5106f
kamercer <kyle.mercer.km@gmail.com>
2015-09-26 14:39:01 -0400

    ajax set up

--- static/JS/loginJS.js
+++ static/JS/loginJS.js
@@ -8,4 +8,15 @@ function verify(){
 	if ($("#flightInputId").val() == "" || $("#emailInputId").val() == ""){
 		alert("Not all fields filled out");
 	}
+	
+	$.ajax({
+		url: dfsdd,
+		type: 'GET',
+		success: function(data){
+			alert(data)
+		},
+		error: function(error){
+			alert(error);
+		}
+	});
 }
