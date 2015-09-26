cddce8b94a9e735d53e8abf0fe84f972c1377c65
AnishN <anish.narayanan32@gmail.com>
2015-09-26 11:39:44 -0400

    Reorganized html and css into templates and static folder

--- hello.py
+++ hello.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
+
+@app.route("/login")
+def hello():
+    return render_template("login.html")
+    
+@app.route("/chat")
+def hello():
+    return render_template("chat.html")
 
 if __name__ == "__main__":
     app.run()
