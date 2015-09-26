edd8b13ea65f8b11b4b777bfd30f2d247a9d367e
Lord and Master of All Things Big and Small <root@turing.iamdevnull.info>
2015-09-26 11:56:14 -0400

    all paths working now

--- hello.py
+++ hello.py
@@ -5,15 +5,15 @@ from flask import request
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def hello1():
     return render_template("index.html")
 
 @app.route("/login")
-def hello():
+def hello2():
     return render_template("login.html")
     
 @app.route("/chat")
-def hello():
+def hello3():
     return render_template("chat.html")
 
 if __name__ == "__main__":
