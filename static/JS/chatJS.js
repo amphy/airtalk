978b611cbb84b50d2163a28f437dc7d6b9407d91
kamercer <kyle.mercer.km@gmail.com>
2015-09-26 20:16:25 -0400

    front end

---
+++ static/JS/chatJS.js
@@ -0,0 +1,25 @@
+function init(){
+	var socket = io.connect('23.94.96.224');
+	
+	socket.on("update", function(msg){
+		if (ready){
+			alert(msg);
+		}
+	});
+	
+	socket.on("update-people", function(people){
+		if (ready){
+			alert(people);
+		}
+	});
+	
+	socket.on("chat", function(who, msg){
+		if (ready){
+			alert(who + "  " + msg);
+		}
+	});
+	
+	socket.on("disconnect", function(){
+		alert("disconnect");
+	})
+}
